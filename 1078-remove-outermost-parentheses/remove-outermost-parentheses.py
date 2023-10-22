4ee17e9cbb44812afb9b8e0b6461e61c869ab78b
Anu <118041314+Anukumari06@users.noreply.github.com>
2023-10-22 21:02:48 +0530

    Time: 41 ms (73.76%) | Memory: 16.2 MB (97.65%) - LeetSync

---
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.py
@@ -0,0 +1,17 @@
+class Solution:
+    def removeOuterParentheses(self, s: str) -> str:
+        ans=""
+        open=0
+        close=0
+        start=0
+        for i in range(0,len(s)):
+            if s[i]=='(':
+                open+=1
+            elif s[i]==')':
+                close+=1
+            if open==close:
+                ans=ans+s[start+1:i]
+                start=i+1
+                close=0
+                open=0
+        return ans
