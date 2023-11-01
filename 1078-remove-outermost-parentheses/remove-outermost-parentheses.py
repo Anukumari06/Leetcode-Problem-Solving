f22d3fb8d1d543c5843d34d72066f717d312dda9
Anu <118041314+Anukumari06@users.noreply.github.com>
2023-11-01 15:00:59 +0530

    Time: 46 ms (40.14%) | Memory: 16.2 MB (83.22%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.py
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.py
@@ -5,13 +5,14 @@ class Solution:
         close=0
         start=0
         for i in range(0,len(s)):
-            if s[i]=='(':
+            if(s[i]=="("):
                 open+=1
-            elif s[i]==')':
+            elif(s[i]==")"):
                 close+=1
-            if open==close:
+            if(open==close):
+
                 ans=ans+s[start+1:i]
                 start=i+1
-                close=0
                 open=0
+                close=0
         return ans
